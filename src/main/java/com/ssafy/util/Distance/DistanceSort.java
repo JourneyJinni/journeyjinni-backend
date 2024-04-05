641b216a7b0f96bcfae58ba2ad756efea1aa1d3c
ssafy <kyuhung1001@gmail.com>
2024-04-05 15:11:52 +0900

    Feat : DistanceSort 구현

--- src/main/java/com/ssafy/util/Distance/DistanceSort.java
+++ src/main/java/com/ssafy/util/Distance/DistanceSort.java
@@ -1,16 +1,34 @@
 package com.ssafy.util.Distance;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
+import org.w3c.dom.Attr;
+
+import com.mysql.cj.x.protobuf.MysqlxCrud.Collection;
+import com.ssafy.mvc.model.AttractionDto;
 
 public class DistanceSort {
 	
-	public static <T> List<T> isAroundSort (double aroundDistance, T attractions) {
-		List<T> aroundList = new ArrayList<>();
+	public static List<AttractionDto> isAroundSort (double nowLat, double nowlon, List<AttractionDto> attractions) {
+		List<AttractionDistance> aroundList = new ArrayList<>();
+		List<AttractionDto> attractionList = new ArrayList<>();
 		
-		// TODO #1 : 객체에서 위도와 경도를 가져오기 
+		// 각 리스트를 순회하며 리스트에 추가 
+		for (AttractionDto attractionDto : attractions) {
+			double d = DistanceUtil.getDistance(nowLat, nowlon, attractionDto.getLatitude().doubleValue(), attractionDto.getLongitude().doubleValue());
+			aroundList.add(new AttractionDistance(attractionDto, d));
+		}
 		
-	    
-	   return aroundList;
+		Collections.sort(aroundList);
+		
+		for (int i = 0; i < aroundList.size(); i++) {
+			System.out.println(aroundList.get(i).getAttraction().getTitle() + "의 위도 경도 : " + aroundList.get(i).getAttraction().getLongitude() + ", " +aroundList.get(i).getAttraction().getLatitude() );
+			System.out.println( "거리 : " + aroundList.get(i).getDistance() + "km");
+			attractionList.add(aroundList.get(i).getAttraction());
+		}
+
+	   return attractionList;
 	}
 }
