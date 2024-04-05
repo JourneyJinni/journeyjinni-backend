81ea43cd440cb4826c67352c9231050de0dc3dc9
ssafy <kyuhung1001@gmail.com>
2024-04-05 13:29:14 +0900

    chore : 거리구하는 getDistance static으로 변경

--- src/main/java/com/ssafy/util/Distance/DistanceUtil.java
+++ src/main/java/com/ssafy/util/Distance/DistanceUtil.java
@@ -8,12 +8,11 @@ public class DistanceUtil {
 	
 	private static final double EARTH_RADIUS_KM = 6371.0; // 지구 둘레
 
-	
 	/**
 	 * 두 위도와 경도 사이의 거리를 구하는 메서드
 	 * @return d는 두 경도 사이의 거리이다.
 	 */
-	private double getDistance(double lat1, double lon1, double lat2, double lon2) {
+	private static double getDistance(double lat1, double lon1, double lat2, double lon2) {
 		double dLat = Math.toRadians(lat2 - lat1);
 	    double dLon = Math.toRadians(lon2 - lon1);
 	    
