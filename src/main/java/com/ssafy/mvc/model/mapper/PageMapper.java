66db7f43e8e1ff9a47e13681febde4034c858690
unggu0704 <kyuhung1001@gmail.com>
2024-05-07 17:12:32 +0900

    Feat : Pagemapper 및 mapper SQL 추가

---
+++ src/main/java/com/ssafy/mvc/model/mapper/PageMapper.java
@@ -0,0 +1,12 @@
+package com.ssafy.mvc.model.mapper;
+
+import com.ssafy.mvc.model.AttractionDto;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface PageMapper {
+
+    List<AttractionDto> getAttractionDtos(int attractionCount);
+}
