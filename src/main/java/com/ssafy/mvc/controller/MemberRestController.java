b61258d790ee3f3d9e8b66adb0998eb9b9a8b403
unggu0704 <kyuhung1001@gmail.com>
2024-05-10 13:42:53 +0900

    Feat : MemberRestController 코드 작성

---
+++ src/main/java/com/ssafy/mvc/controller/MemberRestController.java
@@ -0,0 +1,68 @@
+package com.ssafy.mvc.controller;
+
+import com.ssafy.mvc.model.MemberDto;
+import com.ssafy.mvc.model.service.MemberService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+public class MemberRestController {
+
+	private final MemberService memberService;
+
+	public MemberRestController(MemberService memberService) {
+		this.memberService = memberService;
+	}
+	
+	@PostMapping("/login")
+	protected ResponseEntity<Object> login(@RequestBody Map<String,String> map , HttpSession session) {
+
+		try {
+			MemberDto memberDto = memberService.memberLogin(map);
+			System.out.println("member : " + memberDto);
+			if(memberDto == null) {
+				return ResponseEntity.badRequest()
+						.body(Map.of("success", false, "message", "아이디 또는 비밀번호가 잘못되었습니다."));
+			}
+
+			return ResponseEntity.ok()
+					.body(Map.of("success", true, "message", "로그인에 성공했습니다.", "user", memberDto));
+		} catch(Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body(Map.of("success", false, "message", "서버 오류가 발생했습니다."));
+		}
+
+
+	}
+	
+	@PostMapping("/join")
+	public String join(@RequestParam Map<String,String> map) {
+		
+		System.out.println(map);
+		try {
+			memberService.memberSignUp(map);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "redirect:/";
+	}
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		session.invalidate();
+		return "redirect:/";
+	}
+	@GetMapping("/idcheck/{userid}")
+	@ResponseBody
+    public String idCheck(@PathVariable("userid") String userId) throws Exception {
+		int cnt = memberService.memberIdCheck(userId);
+        return cnt+"";
+
+    }
+}
