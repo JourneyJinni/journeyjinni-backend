cccd167add498fb9586b65ed9f570e411c78ee10
unggu0704 <kyuhung1001@gmail.com>
2024-05-10 12:12:48 +0900

    Feat : AttractionRestController 생성

---
+++ src/main/java/com/ssafy/mvc/controller/AttractionRestController.java
@@ -0,0 +1,156 @@
+package com.ssafy.mvc.controller;
+
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.ssafy.mvc.model.AttractionDto;
+import com.ssafy.mvc.model.NowLocation;
+import com.ssafy.mvc.model.service.AttractionService;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AttractionRestController {
+
+
+    private final AttractionService attractionService;
+
+    @Autowired
+    public AttractionRestController(AttractionService attractionService) {
+        this.attractionService = attractionService;
+    }
+
+
+    /**
+     * 사용자에게 어떤 리스트를 제공할지 결정할 컨트롤러
+     * 프론트 결정 후에 로직 처리
+     * @param city
+     * @param category
+     * @param request
+     * @return
+     */
+    @PostMapping("/filterList")
+    public String filterList(@RequestParam String city, @RequestParam(name = "category") String category,
+                             HttpServletRequest request) {
+
+        Map<String, Object> map = new HashMap<>();
+
+        for (String value : request.getParameterValues("city")) {
+            map.put("cities", value);
+        }
+
+        for (String value : request.getParameterValues("category")) {
+            map.put("categorys", value);
+        }
+
+        try {
+            List<AttractionDto> filteredList = attractionService.getFilteredList(map);
+            System.out.println("[Log] : filterList 실행");
+            request.setAttribute("filteredList", filteredList);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return "tour/attraction";
+    }
+
+    @PostMapping("/nowLocation")
+    public String nowLocation(HttpServletRequest request) {
+        System.out.println("[Log] : 위치 정보가 사용됨!");
+        String latitude = request.getParameter("latitude");
+        String longitude = request.getParameter("longitude");
+        new NowLocation(latitude, longitude);
+
+        return "attraction";
+    }
+
+
+
+//    @Override
+//    public void init(ServletConfig config) throws ServletException {
+//        super.init(config);
+//        attractionService = AttractionServiceImpl.getAttractionService();
+//    }
+//    @Override
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+//            throws ServletException, IOException {
+//        String action = request.getParameter("action");
+//        try {
+//            if ("list".equals(action)) {
+//                List<AttractionDto> attractionList=attractionService.list();
+//
+//                request.setAttribute("attractionList", attractionList);
+//                forward(request, response, "/index.jsp");
+//
+//            } else if ("filterList".equals(action)) {
+//                String[] cities = request.getParameterValues("city");
+//                String[] categorys = request.getParameterValues("category");
+//
+//                List<AttractionDto> filteredList = attractionService.getFilteredList(cities, categorys);
+//
+//                request.setAttribute("filteredList", filteredList);
+//                forward(request, response, "/view/tour/attraction.jsp");
+//            } else if ("attractionDetail".equals(action)) {
+//                String contentId = request.getParameter("selectedAttractionId");
+//
+//                AttractionDto attractionDetail = attractionService.getAttraction(Integer.parseInt(contentId));
+//                request.setAttribute("attractionDetail", attractionDetail);
+//                forward(request, response, "/view/tour/attractiondetail.jsp");
+//            } else if("attractionRoute".equals(action)) {
+//            	ArrayList<String[]> list = new ArrayList<>();
+//            	String[] checked = request.getParameterValues("attractionCheckbox");
+//            	for(int i=0;i<checked.length;i++) {
+//            		String[] input = request.getParameter("attration"+checked[i]).split(",");
+//            		if(input !=null) {
+//            			list.add(input);
+//            		}
+//            	}
+//
+//        		List<Integer> route = attractionService.getRoute(list);
+//        		System.out.println(route.toString());
+////            	System.out.println(Arrays.toString(checked));
+////        		for(String[] p : list) {
+////        			System.out.println(p[0] + " " + p[1] + " " + p[2]);
+////        		}
+//            } else if ("nowLocation".equals(action)) {
+//            	System.out.println("[Log] : 위치 정보가 사용됨!");
+//                String latitude = request.getParameter("latitude");
+//                String longitude = request.getParameter("longitude");
+//
+//                new NowLocation(latitude, longitude);
+//                forward(request, response, "/view/tour/attraction.jsp");
+//
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//
+//    }
+//
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+//            throws ServletException, IOException {
+//        request.setCharacterEncoding("utf-8");
+//        doGet(request, response);
+//    }
+//
+//    private void forward(HttpServletRequest request, HttpServletResponse response, String path)
+//            throws ServletException, IOException {
+//        RequestDispatcher dispatcher = request.getRequestDispatcher(path);
+//        dispatcher.forward(request, response);
+//    }
+//
+//    private void redirect(HttpServletRequest request, HttpServletResponse response, String path) throws IOException {
+//        response.sendRedirect(request.getContextPath() + path);
+//    }
+
+}
