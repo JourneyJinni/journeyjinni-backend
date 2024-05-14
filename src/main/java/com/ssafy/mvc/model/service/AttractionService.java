fa2a68df409472d56eb1608a80c10da67f37ef45
서동인 <sdi4676@naver.com>
2024-05-14 14:29:35 +0900

    gugun불러오기 추가

--- src/main/java/com/ssafy/mvc/model/service/AttractionService.java
+++ src/main/java/com/ssafy/mvc/model/service/AttractionService.java
@@ -7,17 +7,19 @@ import java.util.Map;
 
 import com.ssafy.mvc.model.AttractionDto;
 import com.ssafy.mvc.model.CategoryDto;
+import com.ssafy.mvc.model.GugunDto;
 import com.ssafy.mvc.model.SidoDto;
 
 public interface AttractionService {
 
 	List<SidoDto> getCities() throws SQLException;
 	List<CategoryDto> getCategories() throws SQLException;
-
+	List<GugunDto> getGugun(String sidoCode) throws SQLException;
 
 	List<AttractionDto> list() throws SQLException;
 	AttractionDto getAttraction(int contentId) throws SQLException;
 
 	public List<AttractionDto> getFilteredList(Map<String, Object> map) throws SQLException;
 	List<Integer> getRoute(ArrayList<String[]> list) throws SQLException;;
+	
 }
